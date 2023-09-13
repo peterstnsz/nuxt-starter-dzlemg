80949ce783724e0676debfc3ad1b80d47aa51d5a
peterstnsz <stanitr@gmail.com>
2023-09-13 10:45:21 +0100

    2 files modified

--- components/Toggle.vue
+++ components/Toggle.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const something = ref({ something: String });
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  props: ['itemName'],
+};
 </script>
 
+
 <template>
-  <div>{{ something }}</div>
+  <input type="checkbox" class="hidden" />
+  <div>
+    <p
+      class="w-[256px] p-4 border bg-yellow-50 rounded-[4px] flex flex-row justify-center items-center hover:bg-indigo-900 hover:text-zinc-50 text-zinc-800 cursor-pointer transition-all"
+    >
+      {{ itemName }}
+    </p>
+  </div>
 </template>
+
+<style scoped>
+.border {
+  border-top: 1px solid var(--indigo-900, #312e81);
+  border-right: 1px solid var(--indigo-900, #312e81);
+  border-bottom: 3px solid var(--indigo-900, #312e81);
+  border-left: 1px solid var(--indigo-900, #312e81);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <MainLayout>
     <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+
+          <Toggle itemName="item 1" />
+      <Toggle itemName="item 2" />
   </MainLayout>
 </template>
 <script setup>

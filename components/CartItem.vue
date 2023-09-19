7d2ada6363cebea9bea588454f50cdc82afc520d
peterstnsz <stanitr@gmail.com>
2023-09-19 20:21:21 +0100

    2 files added

---
+++ components/CartItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-start my-2"></div>
+</template>
+
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+
+const props = defineProps(['product', 'selectedArray']);
+const { product, selectedArray } = toRefs(props);
+
+const emit = defineEmits(['selectedRadio']);
+
+let isHover = ref(false);
+let isSelected = ref(false);
+</script>
